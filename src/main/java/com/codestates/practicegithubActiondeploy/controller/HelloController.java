2881ab283b29ea68a840d0fb44262942dc9eee97
CordJG <wjrmffldglem@gmail.com>
2023-04-05 09:50:40 +0900

    commit

--- src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
@@ -1,10 +1,7 @@
 package com.codestates.practicegithubActiondeploy.controller;
 
 import com.codestates.practicegithubActiondeploy.service.CordJgService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -26,7 +23,7 @@ public class HelloController {
 
 
 
-        return result;
+        return result +"what is it?";
     }
 
     @GetMapping()
